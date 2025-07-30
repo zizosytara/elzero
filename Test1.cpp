360b068f2c6ca4691ccf053dba7baf8ca9432468
zeyad <zizosytara24@gmail.com>
2025-07-30 12:36:00 +0300

    insert by three ways for doubly list

--- Test1.cpp
+++ Test1.cpp
@@ -18,6 +18,38 @@ void insert(int value, int position){
         newNode->prev = NULL;
         newNode->next = NULL;
     }
+    //------------in start-----------//
+    Node *newNode = new Node();
+    newNode->data = value;
+    newNode->prev = NULL;
+    newNode->next = head;
+    head->prev = newNode;
+    head = newNode;
+    //-------------in mid------------//
+    Node *newNode = new Node();
+    newNode->data = value;
+    newNode->prev = NULL;
+    newNode->next = NULL;
+    Node *temp = head;
+    for(int i = position-1 ; i > 1 ; i--){
+        temp = temp->next;
+    }
+    newNode->prev = temp;
+    newNode->next = temp->next;
+    (temp->next)->prev = newNode;//count function
+    temp->next = newNode;
+    //-------------in last-----------//
+    Node *newNode = new Node();
+    newNode->data = value;
+    newNode->prev = NULL;
+    newNode->next = NULL;
+    Node *temp = head;
+    for(int i = position-1 ; i > 1 ; i--){
+        temp = temp->next;
+    }
+    newNode->prev = temp;
+    newNode->next = temp->next;
+    temp->next = newNode;
     
 }
 void insertStart(int value){
@@ -50,6 +82,15 @@ void insertBack(int value){
     temp->next = newNode;
     newNode->prev = temp;
     newNode->next = NULL;
+}
+int Count(){
+Node *counter = head;
+int numberNodes = 1;
+while(counter->next != NULL){
+    counter = counter->next;
+    numberNodes++;
+}
+return numberNodes;
 }
 void Display(){
     Node *current = head;
